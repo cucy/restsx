4942a9e10bb2eafa0e8ad66a422c72a5f898b54e
zrd <292016176@qq.com>
2017-10-23 18:35:38 +0800

    v1 drf使用ModelSerializer实现APIView

--- apps/goods/serializers.py
+++ apps/goods/serializers.py
@@ -3,12 +3,12 @@
 __date__ = '2017/10/23 17:25'
 __author__ = 'zhourudong'
 from rest_framework import serializers
-from django.db.models import Q
+
 
 from goods.models import Goods
 
 
-class GoodsSerializer(serializers.Serializer):
-    name = serializers.CharField(max_length=100,)
-    click_num = serializers.IntegerField(default=0)
-    goods_front_image = serializers.ImageField()
+class GoodsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Goods
+        fields = "__all__"
