2573673c917724c3866358b7f793af68f74b6543
zrd <292016176@qq.com>
2017-10-23 21:03:45 +0800

    v4 drf使用字段排序功能

--- apps/goods/views.py
+++ apps/goods/views.py
@@ -28,9 +28,10 @@ class GoodsListViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
     serializer_class = GoodsSerializer
     pagination_class = GoodsResultsSetPagination
 
-    filter_backends = (DjangoFilterBackend, filters.SearchFilter,)
+    filter_backends = (DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter)
     filter_fields = ('name', 'shop_price')  # 过滤
 
     search_fields = ('^goods_desc', '=name', "goods_brief")  # 搜索   ^开头 =等于
+    ordering_fields = ('sold_num', 'add_time')  # 排序
 
     filter_class = GoodsFilter
