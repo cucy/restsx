436e20509423cbd272e774adae5f081fa6040ab7
zrd <292016176@qq.com>
2017-10-23 18:38:37 +0800

    v2 drf使用ModelSerializer实现APIView 嵌套实例化

--- apps/goods/serializers.py
+++ apps/goods/serializers.py
@@ -4,11 +4,18 @@ __date__ = '2017/10/23 17:25'
 __author__ = 'zhourudong'
 from rest_framework import serializers
 
+from goods.models import Goods, GoodsCategory
 
-from goods.models import Goods
+
+class GoodsCategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = GoodsCategory
+        fields = "__all__"
 
 
 class GoodsSerializer(serializers.ModelSerializer):
+    category = GoodsCategorySerializer()
+
     class Meta:
         model = Goods
         fields = "__all__"
