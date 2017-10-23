9335be7d91a408d6535420721ff5200c1c54bc9d
zrd <292016176@qq.com>
2017-10-23 16:58:49 +0800

    v2 使用django View实现简单的restfulapi model_to_dict

--- apps/goods/views_base.py
+++ apps/goods/views_base.py
@@ -17,13 +17,11 @@ class GoodsListView(View):
         json_list = []
         goods = Goods.objects.all()[:10]
 
-        for good in goods:
-            json_dict = {}
-            json_dict["name"] = good.name
-            json_dict["category"] = good.category.name
-            json_dict["market_price"] = good.market_price
-            json_list.append(json_dict)
-
+        from django.forms.models import model_to_dict
         from django.http import HttpResponse
         import json
-        return HttpResponse(json.dumps(json_list),content_type="application/json" )
+
+        for good in goods:
+            json_dict = model_to_dict(good)
+            json_list.append(json_dict)
+        return HttpResponse(json.dumps(json_list), content_type="application/json")
