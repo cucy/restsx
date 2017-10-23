532e02e9359ccda4156d857d201d676e7ad229c6
zrd <292016176@qq.com>
2017-10-23 19:04:08 +0800

    v3 drf使用自定义分页 取数据

--- apps/goods/views.py
+++ apps/goods/views.py
@@ -1,10 +1,21 @@
 from rest_framework import mixins
 from rest_framework import generics
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 from rest_framework import status
 
 from .serializers import GoodsSerializer
 from .models import Goods
+
+
+class GoodsResultsSetPagination(PageNumberPagination):
+    """
+    商品列表分页类
+    """
+    page_size = 10
+    page_size_query_param = 'page_size' # 每页只取3条数据的第二页 http://127.0.0.1:8000/goods/?page_size=3&q=2
+    page_query_param = "q"  # 后边的 http://xxx/?p=1
+    max_page_size = 100
 
 
 class GoodsListView(generics.ListAPIView):
@@ -13,3 +24,4 @@ class GoodsListView(generics.ListAPIView):
     """
     queryset = Goods.objects.all()
     serializer_class = GoodsSerializer
+    pagination_class = GoodsResultsSetPagination
