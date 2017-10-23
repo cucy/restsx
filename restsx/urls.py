c691adeef4990f5676ce1cf80e604f3d2cf07f82
zrd <292016176@qq.com>
2017-10-23 19:33:22 +0800

    v4 drf使用viewsets 注册路由

--- restsx/urls.py
+++ restsx/urls.py
@@ -14,24 +14,24 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url, include
-from django.contrib import admin
 from rest_framework.documentation import include_docs_urls
+from rest_framework.routers import DefaultRouter
 
 import xadmin
 from goods.views import GoodsListViewSet
 from restsx.settings import MEDIA_ROOT
 from django.views.static import serve
 
-goods_list = GoodsListViewSet.as_view({
-    'get': 'list'  # 直接将get请求绑定到list方法上
-    # 'post': 'create'
-})
+router = DefaultRouter()
+
+# 配置goodurl
+router.register(r'goods', GoodsListViewSet)
 
 urlpatterns = [
     url(r'^xadmin/', xadmin.site.urls),
     url(r'^media/(?P<path>.*)$', serve, {"document_root": MEDIA_ROOT}),
 
-    url(r'^goods/', goods_list, name="goods-list"),
+    url(r'^', include(router.urls)),
     url(r'^docs/', include_docs_urls(title="慕学生鲜")),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
