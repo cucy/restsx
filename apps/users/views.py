a090f57d979b5fbbe34cef4d1d5a5c4d4935ff22
zrd <292016176@qq.com>
2017-10-26 21:03:28 +0800

    user 登录注册

--- apps/users/views.py
+++ apps/users/views.py
@@ -100,19 +100,19 @@ class UserViewset(CreateModelMixin, mixins.UpdateModelMixin, mixins.RetrieveMode
     #
     #     return []
     #
-    # def create(self, request, *args, **kwargs):
-    #     serializer = self.get_serializer(data=request.data)
-    #     serializer.is_valid(raise_exception=True)
-    #     user = self.perform_create(serializer)
-    #
-    #     re_dict = serializer.data
-    #     payload = jwt_payload_handler(user)
-    #     re_dict["token"] = jwt_encode_handler(payload)
-    #     re_dict["name"] = user.name if user.name else user.username
-    #
-    #     headers = self.get_success_headers(serializer.data)
-    #     return Response(re_dict, status=status.HTTP_201_CREATED, headers=headers)
-    #
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = self.perform_create(serializer)
+
+        re_dict = serializer.data
+        payload = jwt_payload_handler(user)
+        re_dict["token"] = jwt_encode_handler(payload)
+        re_dict["name"] = user.name if user.name else user.username
+
+        headers = self.get_success_headers(serializer.data)
+        return Response(re_dict, status=status.HTTP_201_CREATED, headers=headers)
+
     # def get_object(self):
     #     return self.request.user
     #
